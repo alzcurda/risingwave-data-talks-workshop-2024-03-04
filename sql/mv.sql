ed6e4bb1d660771c00e3f65dcfef07d0a2888a2a
Alzueta, Daniel <daniel.alzueta@i2cat.net>
2024-03-11 23:13:40 +0100

    Final version

--- sql/mv.sql
+++ sql/mv.sql
@@ -17,8 +17,54 @@ CREATE MATERIALIZED VIEW mv_time_zones_statictics AS
 	FROM mv_time_zones
 	group by zone_in, zone_out
 	
-select * 
-from mv_time_zones_statictics
-order by avg_time desc
+    WITH t AS (
+        SELECT MAX(avg_time) AS max_avg_time
+        FROM mv_time_zones_statictics
+    )
+    SELECT tzs.*
+    FROM mv_time_zones_statictics tzs
+    JOIN t
+        ON tzs.avg_time = t.max_avg_time
+        
+        
+   -- Question 2    
+    
+-- Statistics for time zones	
+CREATE MATERIALIZED VIEW mv_time_zones_statictics2 AS
+	SELECT zone_in
+	, zone_out
+	, avg(time) avg_time
+	, min(time) min_time
+	, max(time) max_time
+	, count(1)  number_trips
+	FROM mv_time_zones
+	group by zone_in, zone_out 
+	
+
+    WITH t AS (
+        SELECT MAX(avg_time) AS max_avg_time
+        FROM mv_time_zones_statictics
+    )
+    SELECT tzs.*
+    FROM mv_time_zones_statictics2 tzs
+    JOIN t
+        ON tzs.avg_time = t.max_avg_time
+        
+        
+    -- Question 3
+    WITH T AS (
+	 	select MAX(td.tpep_pickup_datetime) as MAX_PICKUP_DATETIME
+	 	from trip_data td
+	 	)
+	SELECT tz.zone zone_in	
+		,COUNT(1)
+	FROM trip_data td
+	JOIN taxi_zone tz ON td.puLocationID = tz.location_id
+	join T on TD.tpep_pickup_datetime between (MAX_PICKUP_DATETIME- interval '17 HOUR') AND MAX_PICKUP_DATETIME
+	group by zone_in
+	order by count(1) desc
+        
+        
+        
 
    
